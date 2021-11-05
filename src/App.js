99c4441ec58aa03b5aa3ec1e156f4ae4a62219d4
Nemanja Manojlovic <manojlovic911@hotmail.com>
2021-11-05 19:20:41 +0100

    Add routes for the app's comp pages

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import {Route, Routes} from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
     return (
         <div>
-            <MainHeader />
+            <MainHeader/>
             <main>
                 <Routes>
-                    <Route path={"/welcome"} element={<Welcome/>}/>
-                    <Route path={"/products"} element={<Products/>}/>
+                    <Route path={"/welcome/*"} element={<Welcome/>}/>
+                    <Route path={"/products"} element={<Products/>} exact/>
+                    <Route path={"/products/:productId"} element={<ProductDetail/>}/>
                 </Routes>
             </main>
         </div>
