91df095723f3591931b7f9e6817b3281cc255adb
Nemanja Manojlovic <manojlovic911@hotmail.com>
2021-11-05 19:19:41 +0100

    Create Products and Welcome comp

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,6 +1,14 @@
+import {Route, Routes} from 'react-router-dom';
 
 const Welcome = () => {
-    return (<h1>The Welcome Page</h1>)
+    return (
+        <section>
+            <h1>The Welcome Page</h1>
+            <Routes>
+                <Route path={"new-user"} element={<p>Welcome New User</p>}/>
+            </Routes>
+        </section>
+    )
 };
 
 export default Welcome;
